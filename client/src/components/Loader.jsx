6501c0274d13d916424dea2d4d66269ef04dbbb7
Naman-Parlecha <namanparlecha@gmail.com>
2024-01-27 13:57:48 +0530

    Completed Welcome Page

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,4 +1,6 @@
 export default function Loader() {
-    return <h1>Loader</h1>;
+    return <div className="flex justify-center items-center py-3">
+      <div className="animate-spin rounded-full h-24 w-24 border-red-700 border border-b-2"></div>
+    </div>;
   }
   
